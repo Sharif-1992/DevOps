2f999d828663c377534971e064948f338e0c38fe
sharif <mondal.sharif@gmail.com>
2025-10-15 20:19:45 +0530

    avd dashbaord code

---
+++ avd-dashboard.tf
@@ -0,0 +1,61 @@
+variable "md_content" {
+  description = "Content for the MD tile"
+  default     = "# Hello all champs :)"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "mygroup"
+  location = "West Europe"
+}
+
+resource "azurerm_portal_dashboard" "my-board" {
+  name                = "my-cool-dashboard"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  tags = {
+    source = "terraform"
+  }
+  dashboard_properties = jsonencode({
+   "lenses": {
+        "0": {
+            "order": 0,
+            "parts": {
+                "0": {
+                    "position": {
+                        "x": 0,
+                        "y": 0,
+                        "rowSpan": 2,
+                        "colSpan": 3
+                    },
+                    "metadata": {
+                        "inputs": [],
+                        "type": "Extension/HubsExtension/PartType/MarkdownPart",
+                        "settings": {
+                            "content": {
+                                "settings": {
+                                    "content": "${var.md_content}",
+                                    "subtitle": "",
+                                    "title": ""
+                                }
+                            }
+                        }
+                    }
+                },               
+            }
+        }
+    },
+    "metadata": {
+        "model": {
+            "timeRange": {
+                "value": {
+                    "relative": {
+                        "duration": 24,
+                        "timeUnit": 1
+                    }
+                },
+                "type": "MsPortalFx.Composition.Configuration.ValueTypes.TimeRange"
+            },
+        }
+    }
+})
+}
---
+++ providers.tf
@@ -0,0 +1,25 @@
+terraform { 
+  cloud { 
+    
+    organization = "RHM_Digital_Showroom" 
+
+    workspaces { 
+      name = "rhm-digital-workspace" 
+    } 
+  } 
+}
+
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = ">= 3.70.0" }
+    azuread = { source = "hashicorp/azuread", version = ">= 2.45.0" }
+    random  = { source = "hashicorp/random" }
+  }
+}
+
+
+
+provider "azurerm" {
+  features {}
+}
