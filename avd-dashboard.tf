6b0c017c43d5fd7989cc65039e40e5da7602a163
sharif <mondal.sharif@gmail.com>
2025-10-17 20:32:46 +0530

    working part name

--- avd-dashboard.tf
+++ avd-dashboard.tf
@@ -11,80 +11,72 @@ resource "azurerm_portal_dashboard" "my-board" {
     source = "terraform"
   }
   dashboard_properties = jsonencode({
-   "lenses": {
-        "0": {
-            "order": 0,
-            "parts": {
-                "0": {
-                    "position": {
-                        "x": 0,
-                        "y": 0,
-                        "rowSpan": 2,
-                        "colSpan": 3
-                    },
-                    "metadata": {
-                        "inputs": [],
-                        "type": "Extension/HubsExtension/PartType/MarkdownPart",
-                        "settings": {
-                            "content": {
-                                "settings": {
-                                    "content": "# Hello all :)",
-                                    "subtitle": "",
-                                    "title": ""
-                                }
-                            }
-                        }
-                    }
-                },
-                "1": {
-                    "position": {
-                        "x": 3,
-                        "y": 0,
-                        "rowSpan": 4,
-                        "colSpan": 6
-                    },
-                    "metadata": {
-                        "inputs": [
-                            {
-                                "name": "query",
-                                "value": file("vm_cpu_utilization.kql")
-                            },
-                            {
-                                "name": "resourceType",
-                                "value": "microsoft.operationalinsights/workspaces"
-                            },
-                            {
-                                "name": "resourceId",
-                                "value": azurerm_log_analytics_workspace.log_workspace.id
-                            }
-                        ],
-                        "type": "Extension/HubsExtension/PartType/LogAnalyticsPart",
-                        "settings": {
-                            "content": {
-                                "settings": {
-                                    "query": file("vm_cpu_utilization.kql"),
-                                    "title": "VM CPU Utilization",
-                                    "visualization": "timechart"
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    },
-    "metadata": {
-        "model": {
-            "timeRange": {
-                "value": {
-                    "relative": {
-                        "duration": 24,
-                        "timeUnit": 1
-                    }
-                },
-                "type": "MsPortalFx.Composition.Configuration.ValueTypes.TimeRange"
+    "lenses" : {
+      "0" : {
+        "order" : 0,
+        "parts" : {
+          "0" : {
+            "position" : {
+              "x" : 0,
+              "y" : 0,
+              "rowSpan" : 2,
+              "colSpan" : 3
             },
+            "metadata" : {
+              "inputs" : [],
+              "type" : "Extension/HubsExtension/PartType/MarkdownPart",
+              "settings" : {
+                "content" : {
+                  "settings" : {
+                    "content" : var.md_content,
+                    "subtitle" : "",
+                    "title" : ""
+                  }
+                }
+              }
+            }
+          },
+          "1" : {
+            "position" : {
+              "x" : 3,
+              "y" : 0,
+              "rowSpan" : 4,
+              "colSpan" : 6
+            },
+            "metadata" : {
+              "inputs" : [],
+              "type" : "Extension/Microsoft_OperationsManagementSuite_Workspace/PartType/LogsDashboardPart",
+              "settings" : {
+                "workspace" : {
+                  "id" : azurerm_log_analytics_workspace.log_workspace.id
+                },
+                "Query" : file("vm_cpu_utilization.kql"),
+                "Title" : "VM CPU Utilization",
+                "PartTitle" : "VM CPU Utilization (Logs)",
+                "Subtitle" : "",
+                "QueryVisualization" : {
+                  "chartType" : 3,
+                  "graphSubType" : 1,
+                  "legend" : "hidden"
+                }
+              }
+            }
+          }
         }
+      }
+    },
+    "metadata" : {
+      "model" : {
+        "timeRange" : {
+          "value" : {
+            "relative" : {
+              "duration" : 24,
+              "timeUnit" : 1
+            }
+          },
+          "type" : "MsPortalFx.Composition.Configuration.ValueTypes.TimeRange"
+        },
+      }
     }
-})
+  })
 }
